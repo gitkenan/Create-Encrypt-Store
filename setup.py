50bab74c1759b472f8e56a83f348250747c923a1
Kenan Al-Shamie <kenan.mth@gmail.com>
2019-09-30 14:29:31 +0100

    setup changes

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ config = {
     'description': 'Useful in creating and storing passwords',
     'author': 'Kenan Al-Shamie',
     'url': 'URL to get it at.',
-    'download_url': 'Where to download it.',
+    'download_url': 'https://github.com/gitkenan/Create-Encrypt-Store',
     'author_email': 'ken.alshamie@gmail.com',
     'version': '0.1',
-    'install_requires': ['nose', 'string', 'random'],
+    'install_requires': ['nose', 'string', 'random', 'time'],
     'packages': ['Create Encrypt Store'],
     'scripts': ['docs/password_gen',
-                'docs/caesar_shift'],
+                'docs/caesar_shift'
+                'docs/create_encrypt_store'],
     'name': 'Create Encrypt Store'
 }
 
